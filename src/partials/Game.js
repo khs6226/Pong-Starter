228a78d88e9837e3ebf69ef47b01292be9b35d96
khs6226 <khsun6226@naver.com>
2019-05-07 20:41:44 -0700

    added viewbox using Javascript

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,9 +1,15 @@
+import {SVG_NS} from '../settings';
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
-
+    let svg = document.createElementNS(SVG_NS, 'svg');
+    this.gameElement = document.getElementById(this.element);
+    svg.setAttributeNS(null, "width", this.width);
+    svg.setAttributeNS(null, "height", this.height);
+    svg.setAttributeNS(null, "viewbox", `0 0 ${this.width} ${this.height}`);
+    this.gameElement.appendChild(svg);
 		// Other code goes here...
   }
 
